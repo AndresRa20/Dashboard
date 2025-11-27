3550d5fa9a5f545f17789ee1e5b81088032cf685
AndresRa20 <ar370948@gmail.com>
2025-11-27 16:38:20 -0600

    cambio

--- Scripts/server.js
+++ Scripts/server.js
@@ -119,5 +119,6 @@ app.get("/health", (req, res) => res.status(200).send("OK"));
 const PORT = process.env.PORT || 3000;
 
 initDb().then(() => {
-    app.listen(8080, () => console.log(`Server corriendo en puerto ${PORT}`));
+    app.listen(PORT,"0.0.0.0", () => 
+        console.log("Server corriendo en puerto"+ PORT));
 });
