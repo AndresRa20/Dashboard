46ca44f0f0d875f14f8883c5ad6938522ffe1e1c
AndresRa20 <ar370948@gmail.com>
2025-12-05 18:27:14 -0600

    boton cerrarsesion

--- Scripts/actualizar.js
+++ Scripts/actualizar.js
@@ -12,7 +12,7 @@ document.getElementById('btn-cerrar').addEventListener('click', () => {
   window.close(); // Intentar cerrar la ventana actual
 }); 
 function closeWindow() {
-  localStorage.clear(); // Limpiar almacenamiento local
+  localStorage.removeItem("token"); // Limpiar almacenamiento local
   window.location.href = "/login.html" ; // devuleve al login
 }
   
