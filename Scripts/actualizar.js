5fdc6cc368b80326216ddc81a690e78d73762966
AndresRa20 <ar370948@gmail.com>
2025-12-05 18:14:29 -0600

    modificacion

--- Scripts/actualizar.js
+++ Scripts/actualizar.js
@@ -7,3 +7,12 @@ botonActualizar.addEventListener('click', () => {
     frame.src = src; // Forzar recarga de cada iframe
   });
 });
+
+document.getElementById('btn-cerrar').addEventListener('click', () => {
+  window.close(); // Intentar cerrar la ventana actual
+}); 
+function closeWindow() {
+  localStorage.clear(); // Limpiar almacenamiento local
+  window.location.href = "/login.html" ; // devuleve al login
+}
+  
